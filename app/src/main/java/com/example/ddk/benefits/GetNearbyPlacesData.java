fd17fe486a78855ac2d4bc7a85835dd8817748c1
Mitchell Tran <mitchett@uci.edu>
2018-03-17 21:40:55 -0700

    map actually works

---
+++ app/src/main/java/com/example/ddk/benefits/GetNearbyPlacesData.java
@@ -0,0 +1,71 @@
+package com.example.ddk.benefits;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+
+class GetNearbyPlacesData extends AsyncTask<Object, String, String> {
+
+    private String googlePlacesData;
+    private GoogleMap mMap;
+    String url;
+
+    @Override
+    protected String doInBackground(Object... objects){
+        mMap = (GoogleMap)objects[0];
+        url = (String)objects[1];
+
+        DownloadURL downloadURL = new DownloadURL();
+        try {
+            googlePlacesData = downloadURL.readUrl(url);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return googlePlacesData;
+    }
+
+    @Override
+    protected void onPostExecute(String s){
+
+        List<HashMap<String, String>> nearbyPlaceList;
+        DataParser parser = new DataParser();
+        nearbyPlaceList = parser.parse(s);
+        Log.d("nearbyplacesdata","called parse method");
+        showNearbyPlaces(nearbyPlaceList);
+        Log.d("nearbyplacesdata","finished shownearbyPlaces");
+    }
+
+    private void showNearbyPlaces(List<HashMap<String, String>> nearbyPlaceList)
+    {
+        for(int i = 0; i < nearbyPlaceList.size(); i++)
+        {
+
+            MarkerOptions markerOptions = new MarkerOptions();
+            HashMap<String, String> googlePlace = nearbyPlaceList.get(i);
+
+            String placeName = googlePlace.get("place_name");
+            double lat = Double.valueOf( googlePlace.get("lat"));
+            double lng = Double.valueOf( googlePlace.get("lng"));
+
+
+            LatLng latLng = new LatLng( lat, lng);
+            markerOptions.position(latLng);
+            markerOptions.title(placeName);
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+            Log.d("nearbyplacesdata",Double.toString(lat));
+            mMap.addMarker(markerOptions);
+        }
+    }
+
+}
--- app/src/main/java/com/example/ddk/benefits/MapFragment.java
+++ app/src/main/java/com/example/ddk/benefits/MapFragment.java
@@ -1,25 +1,33 @@
 package com.example.ddk.benefits;
 
-
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
+import java.io.IOException;
 
 
-public class MapFragment extends Fragment{
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
-    SupportMapFragment mapFragment;
+    SupportMapFragment mapFrag;
+    GoogleMap mMap;
+    MarkerOptions currentLocation;
+    double latitude;
+    double longitude;
+    int PROXIMITY_RADIUS = 5093;
 
     public MapFragment() {
         // Required empty public constructor
@@ -29,13 +37,117 @@ public class MapFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, null);//container, false);
+        View v = inflater.inflate(R.layout.fragment_map, null);
+        if(this.mapFrag == null){
+            mapFrag = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        }
+
+        Button gym_button = (Button) v.findViewById(R.id.B_gyms);
+        Button parks_button = (Button) v.findViewById(R.id.B_parks);
+        Button trails_button = (Button) v.findViewById(R.id.B_trails);
+
+        gym_button.setOnClickListener(buttonClick);
+        parks_button.setOnClickListener(buttonClick);
+        trails_button.setOnClickListener(buttonClick);
+
+        mapFrag.getMapAsync(this);
+        return v;
     }
+
+    View.OnClickListener buttonClick = new View.OnClickListener() {
+        @Override
+        public void onClick(final View v) {
+            Object dataTransfer[] = new Object[2];
+            GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
+
+            switch (v.getId()) {
+                case R.id.B_gyms:
+                    mMap.clear();
+                    String url = getUrl(latitude, longitude, "gym");
+                    dataTransfer[0] = mMap;
+                    dataTransfer[1] = url;
+
+                    getNearbyPlacesData.execute(dataTransfer);
+                    Toast.makeText(getActivity(), "Showing Nearby Gyms", Toast.LENGTH_SHORT).show();
+
+                    mMap.addMarker(currentLocation);
+                    break;
+
+
+                case R.id.B_parks:
+                    mMap.clear();
+                    url = getUrl(latitude, longitude, "park");
+                    dataTransfer[0] = mMap;
+                    dataTransfer[1] = url;
+
+                    getNearbyPlacesData.execute(dataTransfer);
+                    Toast.makeText(getActivity(), "Showing Nearby Parks", Toast.LENGTH_SHORT).show();
+
+                    mMap.addMarker(currentLocation);
+                    break;
+
+                case R.id.B_trails:
+                    mMap.clear();
+                    url = getUrl(latitude, longitude, "trail");
+                    dataTransfer[0] = mMap;
+                    dataTransfer[1] = url;
+
+                    getNearbyPlacesData.execute(dataTransfer);
+                    Toast.makeText(getActivity(), "Showing Nearby Trails", Toast.LENGTH_SHORT).show();
+
+                    mMap.addMarker(currentLocation);
+                    break;
+
+            }
+        }
+    };
+
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in current Location
+        latitude = 33.6416246;
+        longitude = -117.8475188;
+        LatLng curloc = new LatLng(latitude, longitude);
+        currentLocation = new MarkerOptions()
+                                        .position(curloc)
+                                        .title("Current Location");
+        currentLocation.icon(BitmapDescriptorFactory.fromResource(R.drawable.cur_loc));
+        mMap.addMarker(currentLocation);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(curloc));
+        mMap.animateCamera( CameraUpdateFactory.zoomTo( 15.0f ) );
+    }
+
+    private String getUrl(double latitude , double longitude , String nearbyPlace)
+    {
+
+        StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        if(nearbyPlace.equals("trail"))
+            googlePlaceUrl.append("name=trail");
+        else
+            googlePlaceUrl.append("type="+nearbyPlace);
+        googlePlaceUrl.append("&location="+latitude+","+longitude);
+        googlePlaceUrl.append("&rankby=distance");
+
+        googlePlaceUrl.append("&key="+"AIzaSyDISwGhSNSNWTcTqrOB6SrRLi10rISiSwo");
+
+
+        return googlePlaceUrl.toString();
+    }
+
+
+
+
 
 }
