f43f9f058107f3523da29f2461905cc35ce5b4c8
Mitchell Tran <mitchett@uci.edu>
2018-03-20 20:58:05 -0700

    map actually works

--- app/src/main/java/com/example/ddk/benefits/HomeFragment.java
+++ app/src/main/java/com/example/ddk/benefits/HomeFragment.java
@@ -1,8 +1,10 @@
 package com.example.ddk.benefits;
 
+import android.graphics.Typeface;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -39,6 +41,9 @@ public class HomeFragment extends Fragment {
     private TextView todayView;
     private Button logButton;
 
+    TextView cityField, detailsField, currentTemperatureField, weatherIcon, updatedField;
+    Typeface weatherFont;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +53,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        weatherFont = Typeface.createFromAsset(getContext().getAssets(), "fonts/weathericons-regular-webfont.ttf");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, null);//container, false);
     }
@@ -62,6 +68,27 @@ public class HomeFragment extends Fragment {
         Date todayDate = new Date();
         String dateView = dateFormat.format(todayDate);
         todayView.setText(dateView);
+
+        cityField = getView().findViewById(R.id.city_field);
+        updatedField = getView().findViewById(R.id.updated_field);
+        detailsField = getView().findViewById(R.id.details_field);
+        currentTemperatureField = getView().findViewById(R.id.current_temperature_field);
+        weatherIcon = getView().findViewById(R.id.weather_icon);
+        weatherIcon.setTypeface(weatherFont);
+
+        WeatherAPI.placeIdTask asyncTask =new WeatherAPI.placeIdTask(new WeatherAPI.AsyncResponse() {
+            public void processFinish(String weather_city, String weather_description, String weather_temperature, String weather_humidity, String weather_pressure, String weather_updatedOn, String weather_iconText, String sun_rise) {
+
+                cityField.setText(weather_city);
+                updatedField.setText(weather_updatedOn);
+                detailsField.setText(weather_description);
+                currentTemperatureField.setText(weather_temperature);
+                weatherIcon.setText(Html.fromHtml(weather_iconText));
+
+            }
+        });
+        asyncTask.execute("33.6416246", "-117.8475188");
+
 
         view.findViewById(R.id.logButton).setOnClickListener(new View.OnClickListener() {
             @Override
